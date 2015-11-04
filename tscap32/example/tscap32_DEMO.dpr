4751d55ff8f15a124647d783f40914e83257e7f2
Petr Kundrata <pksoft@atlas.cz>
2015-11-04 16:20:10 +0100

    tscap32 - knihovna pro vytváření videosouborů - staženo a trochu upraveno PChar -> PAnsiChar - ale stejně to nejde zkompilovat - DockForm chybí

---
+++ tscap32/example/tscap32_DEMO.dpr
@@ -0,0 +1,24 @@
+program tscap32_DEMO;
+
+uses
+  Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  tscap32_dt in '..\tscap32_dt.pas',
+  tscap32_rt in '..\tscap32_rt.pas',
+  tsDebug in '..\tsDebug.pas',
+  tsDibRel in '..\tsDibRel.pas',
+  tsLogbox in '..\tsLogbox.pas',
+  tsMessages in '..\tsMessages.pas',
+  tsmisc in '..\tsmisc.pas',
+  tsTechAboutFrm in '..\tsTechAboutFrm.pas',
+  tstlg in '..\tstlg.pas',
+  tstlg2 in '..\tstlg2.pas',
+  vfwunit in '..\vfwunit.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
